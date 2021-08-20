2d5829f1a0a4ea91f40805d901bbf271dbee6d6f
wszqkzqk <wszqkzqk@qq.com>
2021-08-20 14:06:05 +0800

    Updated by update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd `dirname $0`; pwd
 git add *
+git add .gitignore
 git commit -m 'Updated by update.sh'
 git push -u git@gitee.com:wszqkzqk/wszqkzqk.git
 git push -u git@github.com:wszqkzqk/wszqkzqk.github.io.git
