35adcec47c0713dea6089a18e9149ec34dc2e787
wszqkzqk <wszqkzqk@qq.com>
2021-08-20 13:59:43 +0800

    Updated by update.sh

--- _site/git-pull.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd `dirname $0`; pwd
-git pull git@github.com:wszqkzqk/wszqkzqk.github.io.git
--- _site/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd `dirname $0`; pwd
-git add *
-git commit -m 'auto updated by update.sh'
-git push -u git@github.com:wszqkzqk/wszqkzqk.github.io.git
-
