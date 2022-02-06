d5360a83556a04aea56c8f876080c56e2d6c4750
wszqkzqk <wszqkzqk@qq.com>
2022-02-06 10:54:04 +0800

    u

--- mdtools/markdown图片自动webp.py
+++ mdtools/markdown图片自动webp.py
@@ -2,11 +2,28 @@
 import os
 from platform import system
 
-def unixpath(path):
-    return path.replace('\\', '/')
+if system() == 'Linux':    # 需要先安装xclip软件包
+    def clip(info):
+        print(info)
+        os.system('echo "' + info + '" |xclip -selection  c ')
+        print('已复制到剪贴板！')
+    
+    localimg = os.path.dirname(os.path.dirname(__file__)) + '/img'
+elif system() == 'Windows':
+    def clip(info):
+        print(info)
+        os.system('echo "' + info + '" | clip')
+        print('已复制到剪贴板！（可能需要手动去除多余的引号）')
+    
+    def unixpath(path):
+        return path.replace('\\', '/')
+    
+    localimg = unixpath(os.path.dirname(os.path.dirname(__file__))) + '/img'
+else:
+    print('操作系统不受支持！')
+    exit()
 
 print('本程序将在插入图片的同时，将jpg、png、bmp、gif图片转化为webp格式\n')
-localimg = unixpath(os.path.dirname(os.path.dirname(__file__))) + '/img'
 
 def autowebp(imgfile):  # 仅对本地图片使用
     if imgfile.split('.')[-1].lower() in {jpg, png, jpeg, bmp, gif}:
@@ -21,19 +38,6 @@ def autowebp(imgfile):  # 仅对本地图片使用
             os.remove("{0}/{1}".format(localimg, webpimg))
             return '/img/' + imgfile
 
-if system() == 'Linux':    # 需要先安装xclip软件包
-    def clip(info):
-        print(info)
-        os.system('echo "' + info + '" |xclip -selection  c ')
-        print('已复制到剪贴板！')
-elif system() == 'Windows':
-    def clip(info):
-        print(info)
-        os.system('echo "' + info + '" | clip')
-        print('已复制到剪贴板！（可能需要手动去除多余的引号）')
-else:
-    print('操作系统不受支持！')
-    exit()
 while 1:
     print('请输入图片地址（默认在/img下，也可输入绝对网址）：')
     url = input()
