85bfe20a5ab5f66f85f4c914b890f55d12b75837
星外之神 <wszqkzqk@qq.com>
2022-02-07 17:03:57 +0800

    优化自动推送脚本

--- update.sh
+++ update.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 cd `dirname $0`; pwd
+
+# 指定仓库地址
+repoinfo="wszqkzqk/wszqkzqk.github.io.git"
+
+# 添加更改
 git add *
 git add .gitignore
 git commit -m 'Updated by update.sh'
-git pull git@github.com:wszqkzqk/wszqkzqk.github.io.git
-git push -u git@gitee.com:wszqkzqk/wszqkzqk.git
-git push -u git@github.com:wszqkzqk/wszqkzqk.github.io.git
 
+# 合并远端更改
+git pull git@github.com:${repoinfo}
+
+# 检测是否存在镜像仓库，如果不存在，进行添加
+flag=1
+for i in $(git remote);do
+    if [[ ${i} == mirror ]];then
+        flag=0
+    fi
+done
+
+# 如果不存在镜像仓库，进行添加
+if [[ ${flag} == 1 ]];then
+    git remote add mirror git@gitee.com:${repoinfo}
+
+# 进行推送
+git push origin
+git push mirror
