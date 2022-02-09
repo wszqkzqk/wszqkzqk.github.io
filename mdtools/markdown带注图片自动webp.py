b03d210f996cd85c17402ecf473262e8ca633fdf
星外之神 <wszqkzqk@qq.com>
2022-02-09 17:16:14 +0800

    支持webp的无损压缩功能

--- mdtools/markdown带注图片自动webp.py
+++ mdtools/markdown带注图片自动webp.py
@@ -6,53 +6,97 @@ import sys
 def unixpath(path):
     return path.replace('\\', '/')
 
-print('本程序将在插入图片的同时，将jpg、png、bmp、gif图片转化为webp格式\n')
+print('本程序将在插入图片的同时，可以按需将jpg、png、bmp、gif、ico、icon图片转化为webp格式以节省空间与网页流量，并将自动对使用webp进行有损压缩还是无损压缩给出建议\n')
 localimg = os.path.dirname(unixpath(sys.path[0])) + '/img'
 
 def autowebp(imgfile):  # 仅对本地图片使用
-    if imgfile.split('.')[-1].lower() in {'jpg', 'png', 'jpeg', 'bmp', 'gif'}:
+    
+    # 检查该本地文件是否存在
+    if not os.path.exists("{0}/{1}".format(localimg, imgfile)):
+        print('该文件既不是网络文件，又无法在本地找到！请检查输入！')
+        return
+
+    appendname = imgfile.split('.')[-1].lower()
+
+    if appendname in {'png', 'bmp', 'gif', 'ico', 'icon', 'jpg', 'jpeg'}:
+        llwebpimg = imgfile.split('.')[0] + '-lossless.webp'
         webpimg = imgfile.split('.')[0] + '.webp'
-        if not os.path.exists("{0}/{1}".format(localimg, webpimg)):
-            os.system('ffmpeg -i "{0}/{1}" "{0}/{2}"'.format(localimg, imgfile, webpimg))
-            if os.path.getsize("{0}/{1}".format(localimg, imgfile)) / os.path.getsize("{0}/{1}".format(localimg, webpimg)) >= 1.15: # 如果转码前后的图片体积比大于1.15则取webp,否则仍然取之前的图片
-                print('\nwebp格式可以显著减少图片体积，采用webp格式！')
+
+        if os.path.exists("{0}/{1}".format(localimg, llwebpimg)):
+            lltodo = input('检测到目标文件"{}"已存在！\n若需要直接使用已存在文件请输入"0"，覆盖请输入"1"，需要重命名请输入"2"（默认"0"）:\n'.format(llwebpimg))
+            if lltodo == '2':
+                llwebpimg = input('请输入用于无损压缩webp的文件名（默认为originalname-new.webp）：\n')
+            elif lltodo == '0':
+                return '/img/' + llwebpimg
+
+        if os.path.exists("{0}/{1}".format(localimg, webpimg)):
+            lossytodo = input('检测到目标文件"{}"已存在！\n若需要直接使用已存在文件请输入"0"，覆盖请输入"1"，需要重命名请输入"2"（默认"0"）:\n'.format(webpimg))
+            if lossytodo == '2':
+                webpimg = input('请输入用于有损压缩webp的文件名（默认为originalname-new.webp）：\n')
+            elif lossytodo == '0':
+                return '/img/' + webpimg
+
+        # 调用ffmpeg转化
+        os.system('ffmpeg -i "{0}/{1}" "{0}/{2}"'.format(localimg, imgfile, webpimg))
+        os.system('ffmpeg -i "{0}/{1}" -lossless 1 "{0}/{2}"'.format(localimg, imgfile, llwebpimg))
+        
+        originsize = os.path.getsize("{0}/{1}".format(localimg, imgfile))
+        lossysize = os.path.getsize("{0}/{1}".format(localimg, webpimg))
+        llsize = os.path.getsize("{0}/{1}".format(localimg, llwebpimg))
+
+        # 判断无损压缩相比于原图有无压缩效果
+        if originsize / llsize > 1:
+            llok = 1
+        else:
+            llok = 0
+        # 判断有损压缩是否比无损压缩更省体积
+        if llsize > lossysize:
+            if (originsize / lossysize) >= 1.15:
+                lossyok = 1
+            else:
+                lossyok = 0
+        else:
+            lossyok = 0
+
+        if llok and lossyok:
+            preference = (llsize - lossysize)*100 / llsize
+            print('\n完成！检测到无损压缩与有损压缩均能有效减小图片大小！')
+            choice = input('其中有损压缩文件比无损压缩文件体积小{}%\n若保留无损压缩请输入"1"，保留有损压缩请输入"0"（默认"1"）：\n'.format(round(preference, 2)))
+            if choice == '0':
+                os.remove("{0}/{1}".format(localimg, llwebpimg))
                 flag = input('若需要删除源文件请输入"1"，否则请输入"0"（默认"0"）：\n')
                 if flag == '1':
                     os.remove("{0}/{1}".format(localimg, imgfile))
+                print('完成！所选的新图片相比原图节省了{}%的空间'.format(round((originsize - lossysize)*100 / originsize, 2)))
                 return '/img/' + webpimg
             else:
-                print('\nwebp格式没有显著减少图片体积，保留原有格式！')
                 os.remove("{0}/{1}".format(localimg, webpimg))
-                return '/img/' + imgfile
+                flag = input('若需要删除源文件请输入"1"，否则请输入"0"（默认"0"）：\n')
+                if flag == '1':
+                    os.remove("{0}/{1}".format(localimg, imgfile))
+                print('完成！所选的新图片相比原图节省了{}%的空间'.format(round((originsize - llsize)*100 / originsize, 2)))
+                return '/img/' + llwebpimg
+        elif llok:
+            print('检测到无损压缩能有效减小图片大小，采用有损压缩！')
+            os.remove("{0}/{1}".format(localimg, webpimg))
+            flag = input('若需要删除源文件请输入"1"，否则请输入"0"（默认"0"）：\n')
+            if flag == '1':
+                os.remove("{0}/{1}".format(localimg, imgfile))
+            print('完成！所选的新图片相比原图节省了{}%的空间'.format(round((originsize - llsize)*100 / originsize, 2)))
+            return '/img/' + llwebpimg
+        elif lossyok:
+            print('检测到有损压缩能有效减小图片大小，采用无损压缩！')
+            os.remove("{0}/{1}".format(localimg, llwebpimg))
+            flag = input('若需要删除源文件请输入"1"，否则请输入"0"（默认"0"）：\n')
+            if flag == '1':
+                os.remove("{0}/{1}".format(localimg, imgfile))
+            print('完成！所选的新图片相比原图节省了{}%的空间'.format(round((originsize - lossysize)*100 / originsize, 2)))
+            return '/img/' + webpimg
         else:
-            todo = input('检测到已有对应文件名的webp文件存在，若需要直接使用已存在文件请输入"0"，覆盖请输入"1"，需要重命名请输入"2"（默认"0"）:\n')
-            if todo == '1':
-                os.system('ffmpeg -i "{0}/{1}" "{0}/{2}"'.format(localimg, imgfile, webpimg))
-                if os.path.getsize("{0}/{1}".format(localimg, imgfile)) / os.path.getsize("{0}/{1}".format(localimg, webpimg)) >= 1.15: # 如果转码前后的图片体积比大于1.15则取webp,否则仍然取之前的图片
-                    print('webp格式可以显著减少图片体积，采用webp格式！')
-                    flag = input('若需要删除源文件请输入"1"，否则请输入"0"（默认"0"）：\n')
-                    if flag == '1':
-                        os.remove("{0}/{1}".format(localimg, imgfile))
-                    return '/img/' + webpimg
-                else:
-                    print('webp格式没有显著减少图片体积，保留原有格式！')
-                    os.remove("{0}/{1}".format(localimg, webpimg))
-                    return '/img/' + imgfile
-            elif todo == '2':
-                webpimg = input('请输入目标webp文件名（默认为originalname-new.webp）：\n')
-                os.system('ffmpeg -i "{0}/{1}" "{0}/{2}"'.format(localimg, imgfile, webpimg))
-                if os.path.getsize("{0}/{1}".format(localimg, imgfile)) / os.path.getsize("{0}/{1}".format(localimg, webpimg)) >= 1.15: # 如果转码前后的图片体积比大于1.15则取webp,否则仍然取之前的图片
-                    print('webp格式可以显著减少图片体积，采用webp格式！')
-                    flag = input('若需要删除源文件请输入"1"，否则请输入"0"（默认"0"）：\n')
-                    if flag == '1':
-                        os.remove("{0}/{1}".format(localimg, imgfile))
-                    return '/img/' + webpimg
-                else:
-                    print('webp格式没有显著减少图片体积，保留原有格式！')
-                    os.remove("{0}/{1}".format(localimg, webpimg))
-                    return '/img/' + imgfile
-            else:
-                return '/img/' + webpimg
+            print('webp并不能很好地优化文件大小，保留原图！')
+            os.remove("{0}/{1}".format(localimg, webpimg))
+            os.remove("{0}/{1}".format(localimg, llwebpimg))
+            return '/img/' + imgfile
     else:
         return '/img/' + imgfile
 
@@ -68,24 +112,29 @@ for i in range(n):
         if '/img/' not in url:
             if url:
                 url = autowebp(url)
+            else:
+                print('无输入内容！若需要将此位置留空请输入"1"，不需要则请输入"0"（默认"0"）')
+                if input() == '1':
+                    picline += '    |'
+                    tailline += '    |'
         else:
             url = autowebp(os.path.basename(url))
+    while not url:
+        print('请重新输入第{}张图片的地址（默认在/img下，也可输入绝对网址）：'.format(i+1))
+        url = input()
+        if '://' not in url:
+            if '/img/' not in url:
+                if url:
+                    url = autowebp(url)
+            else:
+                url = autowebp(os.path.basename(url))
     print('请输入点击图片的链接指向（默认为本身）')
     goto = input()
     if not goto:
         goto = url
-    if url:
-        picline += '[![#~{0}]({0})]({1})|'.format(url, goto)
-    else:
-        picline += '    |'
+    picline += '[![#~{0}]({0})]({1})|'.format(url, goto)
     print('请输入图注：')
     note = input()
-    if note:
-        tailline += '{}|'.format(note)
-    else:
-        tailline += '    |'
+    tailline += '{}|'.format(note)
 
-print()
-print(picline)
-print(neckline)
-print(tailline)
+print('正在生成符合markdown语法的表格……\n', picline, neckline, tailline, '\n完成！请自行将以上内容复制到所需要的markdown文件内', sep='\n')
